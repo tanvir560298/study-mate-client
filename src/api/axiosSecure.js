62047bd8e5bcf0e23fa0a4b502def9bc8c794333
Tanvir Ahmad <tanvirahmad@Tanvirs-MacBook-Air-2.local>
2026-02-01 11:37:42 -0800

    Update axios baseURL for production

--- src/api/axiosSecure.js
+++ src/api/axiosSecure.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const axiosSecure = axios.create({
-  baseURL: "https://study-mate-server-rho.vercel.app",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
